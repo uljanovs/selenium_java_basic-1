6d0ad414c5f3329e731c6197e36eac49d6f38a79
uljanovs <a.uljanovs@gmail.com>
2020-03-01 16:24:14 +0200

    Update extra1Task.java

--- src/selenium/sample/extra/extra1Task.java
+++ src/selenium/sample/extra/extra1Task.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class extra1Task {
     WebDriver driver;
-    String base_url = "https://kristinek.github.io/site/examples/act";
-    String new_url = "https://kristinek.github.io/site/examples/link1";
+    String base_url = "https://uljanovs.github.io/site/examples/act";
+    String new_url = "https://uljanovs.github.io/site/examples/link1";
 
     // method which is being run before each test
     @Before
@@ -33,27 +33,27 @@ public class extra1Task {
     @Test
     public void navigateBack() throws Exception {
 //        TODO
-//        open page with url "https://kristinek.github.io/site/examples/po"
+//        open page with url "https://uljanovs.github.io/site/examples/po"
 //        click "More > " for the top left element
-//        check that the url now "https://kristinek.github.io/site/examples/po1"
-//        using driver navigation go back to "https://kristinek.github.io/site/examples/po"
-//        check that the page now is "https://kristinek.github.io/site/examples/po"
+//        check that the url now "https://uljanovs.github.io/site/examples/po1"
+//        using driver navigation go back to "https://uljanovs.github.io/site/examples/po"
+//        check that the page now is "https://uljanovs.github.io/site/examples/po"
     }
 
     @Test
     public void navigateForward() throws Exception {
 //        TODO
-//        open page with url "https://kristinek.github.io/site/examples/po"
+//        open page with url "https://uljanovs.github.io/site/examples/po"
 //        click "More > " for the top left element
-//        using driver navigation go back to "https://kristinek.github.io/site/examples/po"
-//        using driver navigation go forward to "https://kristinek.github.io/site/examples/po1"
-//        check that the page now is "https://kristinek.github.io/site/examples/po1"
+//        using driver navigation go back to "https://uljanovs.github.io/site/examples/po"
+//        using driver navigation go forward to "https://uljanovs.github.io/site/examples/po1"
+//        check that the page now is "https://uljanovs.github.io/site/examples/po1"
     }
 
     @Test
     public void refresh() throws Exception {
 //        TODO
-//        open page "https://kristinek.github.io/site/examples/act"
+//        open page "https://uljanovs.github.io/site/examples/act"
 //        click on "Show" button in 'Button' section
 //        check that text "I am here!" is seen
 //        refresh page
